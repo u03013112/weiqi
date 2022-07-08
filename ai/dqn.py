a16a788b4d97a0b0b52a8d86d8098c4c35c36721
u03013112 <u03013112@hotmail.com>
2022-07-08 17:56:58 +0800

    写了一半，dqn还没写完

--- ai/dqn.py
+++ ai/dqn.py
@@ -6,6 +6,11 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
+import sys
+sys.path.append('/ai')
+
+from env import Env
+
 EPISODES = 1000
 
 class DQNAgent:
@@ -13,6 +18,7 @@ class DQNAgent:
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
+        
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
@@ -60,7 +66,7 @@ class DQNAgent:
 
 
 if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
+    env = Env()
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
@@ -71,19 +77,59 @@ if __name__ == "__main__":
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
+        # 每一盘做两套mem，为了可以将奖励往回传
+        memBlack = []
+        memWhite = []
+        lastState = None
+        lastAction = None
+        lastReward = 0
         for time in range(500):
-            # env.render()
+            # color == 0 的时候是黑色的，否则是白色的
+            color = time%2
+
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
-            reward = reward if not done else -10
+            # reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
-            agent.memorize(state, action, reward, next_state, done)
-            state = next_state
+            # agent.memorize(state, action, reward, next_state, done)
+            
+            if lastAction:
+                if color == 0:
+                    # 如果现在是下黑色，那么上一次应该是下白色，并且现在下完黑色的样子是下白色的下一状态
+                    # lastReward是上次白旗下完自己的奖励，这次黑棋的奖励应该是白旗的惩罚，所以要减去
+                    memWhite.append([lastState,lastAction,lastReward - reward,next_state,done])
+                else:
+                    memBlack.append([lastState,lastAction,lastReward - reward,next_state,done])
+            
             if done:
-                print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
+                if color == 0:
+                    # 如果现在是下黑色，那么代表黑色的操作导致游戏结束，直接给这次操作奖励
+                    memBlack.append([state,action,reward,next_state,done])
+                else:
+                    memWhite.append([state,action,reward,next_state,done])
+                # 整理黑白两个mem，计算reward
+                for i in range(len(memBlack)):
+                    if i == 0:
+                        continue
+                    # 这里认为至少会有2步，不会1步认输，所以暂不考虑越界
+                    memBlack[len(memBlack)-1-i][2] += agent.gamma * memBlack[len(memBlack)-i][2]
+                
+                for i in range(len(memWhite)):
+                    if i == 0:
+                        continue
+                    memWhite[len(memWhite)-1-i][2] += agent.gamma * memWhite[len(memWhite)-i][2]
+                # 然后拼到一起以供replay
+                for m in memBlack:
+                    agent.memorize(m[0], m[1], m[2], m[3], m[4])
+                for m in memWhite:
+                    agent.memorize(m[0], m[1], m[2], m[3], m[4])
                 break
-            if len(agent.memory) > batch_size:
-                agent.replay(batch_size)
+
+            lastAction = action
+            lastState = state
+            lastReward = reward
+            state = next_state
+            # if len(agent.memory) > batch_size:
+            #     agent.replay(batch_size)
         # if e % 10 == 0:
         #     agent.save("./save/cartpole-dqn.h5")
--- ai/env.py
+++ ai/env.py
@@ -1,30 +1,75 @@
 # 与core连接，为ai提供env
 # 争取做到和gym类似，这样可以较为快速的试用现成代码看效果
 import requests
+import numpy as np
+
+class ActionSpace:
+    def __init__(self,n):
+        self.n = n
 
 class Env:
     def __init__(self):
-        self.observation_space = []
-        self.action_space = {}
-        self.action_space.n = 64
+        self.observation_space = np.zeros(128)
+        self.action_space = ActionSpace(64)
+
+    # 将core返回的status变换成标准的state，需要做什么转换都在这里进行
+    def getState(self,status):
+        # N个8X8
+        # 暂时不分黑白棋，需要下黑子时候黑色就是主色，白子就是对色；反之亦然
+        # 本色 4个 8X8 ，目前所有，1、2、3口气
+        # 对色 4个 8X8 ，目前所有，1、2、3口气
+        # 暂时不用卷积，先试试看
+        
+        # 重新涂色，根据目前该什么颜色下棋，来确定自己的颜色
+        selfColor = 1
+        oppoColor = 2
+        if status['status'] == 'turn2':
+            selfColor = 2
+            oppoColor = 1
+
+        qipan = status['qipan']
+        # 拆分成8个8x8，共512个输入单元，直接拍平做输入
+        # 1、目前自己所有子
+        qipanNp = np.array(qipan)
+        qipanNpSelf = qipanNp.copy()
+        qipanNpSelf[qipanNpSelf==oppoColor]=0
+        # 2、目前对方所有子
+        qipanNpOppo = qipanNp.copy()
+        qipanNpOppo[qipanNpOppo==selfColor]=0
+        # TODO：暂时先做两个，后面的确实比较麻烦
+        ret = np.concatenate((qipanNpSelf,qipanNpOppo))
+        
+        # 拍平
+        return ret.reshape(-1)
 
     def reset(self):
-        # body = {
-        
-        # }
-        # headers = {
-        #     'Content-Type': 'application/json'
-        # }
-        # r = requests.post('https://hq1.appsflyer.com/api/cohorts/v1/data/app/com.topwar.gp?format=json', json=body,headers=headers)
-        # print(r.json())
         r = requests.get('http://weiqi_core:8080/start')
-        ret = r.json()
-        print(ret)
+        status = r.json()
+        return self.getState(status)
+
+    # 获得所有可以落子的地方，如果是不能落子的地方，就直接给一个惩罚吧
+    def getAllActions(self,status):
+        return
 
     # return next_state, reward, done, _
     def step(self,action):
-        pass
+        # action 是0~63
+        x = action/8
+        y = action%8
+        body = {
+            'x':int(x),
+            'y':int(y)
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        r = requests.post('http://weiqi_core:8080/do', json=body,headers=headers)
+        status = r.json()
+        return self.getState(status)
 
 if __name__ == '__main__':
     env = Env()
     env.reset()
+    env.step(0)
+    env.step(1)
+    env.step(2)
